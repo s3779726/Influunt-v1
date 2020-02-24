9c17347d6ed5575db56e5a3be4a324bdab5b7e58
Austin Lay <49256155+s3779726@users.noreply.github.com>
2020-02-24 15:18:16 +1100

    Complete backend RESTful api

--- Backend/server.js
+++ Backend/server.js
@@ -9,13 +9,12 @@ app.use(express.static("public"));
 mongoose.connect('mongodb://localhost:27017/todoListDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const TaskSchema = new mongoose.Schema({
-    name:String,
-    content:String
+    taskName:String,
 });
 
 const ListSchema = new mongoose.Schema({
     listTitle:String,
-    task:[TaskSchema]
+    tasks:[TaskSchema]
 });
 
 
@@ -24,6 +23,74 @@ const List = mongoose.model("List", ListSchema);
 app.get("/", (req,res) => {
     res.send("hello world");
 });
+
+app.route("/lists").get(function(req,res){
+    List.find({}, function(err,lists){
+        if(err){
+            console.log(err);
+            
+        } else{
+            res.send(lists);
+        }
+    });
+})
+.post(function(req,res){
+    const listTitle = req.body.title;
+    const newList = new List({
+        listTitle:listTitle,
+        tasks:[]
+    });
+
+    newList.save(function(err){
+        if(err){
+            console.log(err);
+            
+        } else{
+            res.send("Successfully added a new List");
+        }
+    });
+
+
+})
+.delete(function(req,res){
+    List.deleteMany({}, function(err){
+        if(err){
+            console.log(err);
+        } else{
+            res.send("successfully deleted all lists");
+        }
+    });
+});
+
+
+app.route("/lists/:listId")
+.get(function(req,res){
+    List.find({_id:req.params.listId}, function(err, list){
+        (err) ? console.log(err) : res.send(list);
+    });
+})
+.patch(function(req,res){  
+    const updateObject = req.body;
+    List.update({ _id:req.params.listId}, { $set: updateObject}, function(err) {
+        (err)?console.log(err): res.send("SUccessfully updated the list");
+        
+      });
+})
+.put(function(req,res){
+    const updateObject = req.body;
+    List.update({ _id:req.params.listId}, {listTitle:updateObject.listTitle}, {overwrite:true}, function(err) {
+        (err)?console.log(err): res.send("Successfully updated the list");
+        
+      });
+})
+.delete(function(req,res){
+    List.deleteOne({ _id: req.params.listId }, function (err) {
+        (err) ? console.log(err): res.send("Sucessfully deleted the list");
+
+      });
+
+});
+
 
 
 
